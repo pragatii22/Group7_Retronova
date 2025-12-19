30d5142b814006720c1443c41081489a79feecdf
shrinakaphle <shrinakaphle@gmail.com>
2025-12-19 12:37:09 +0545

    added category

---
+++ src/model/category.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author shrina
+ */
+
+    
+
+public class category {
+
+    private int categoryId;
+    private String name;
+
+    
+
+    
+    public category(int categoryId, String name) {
+        this.categoryId = categoryId;
+        this.name = name;
+    }
+
+    
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+
